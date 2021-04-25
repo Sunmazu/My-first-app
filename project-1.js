0fcc8320948193d9a3205e660a98124e6f241ca1
Sunmazu <sunm4zu@gmail.com>
2021-04-26 00:45:05 +0200

    Dodany został kalkulator

--- project-1.js
+++ project-1.js
@@ -4,7 +4,7 @@ function alert1(){
     alert('Oto mój pierwszy projekt');
 }
 
-
+// Alert #2
 function alert2(){
 let zdanie='nazywam się Matthiass i mam ';
 let age = 20;
@@ -12,12 +12,48 @@ let zdanie2=' lat, mam nadzieję, że nie załamałeś się wchodząc i widząc 
 alert(zdanie+age+zdanie2);
 }
 
+// Alert #3
 function alert3(){
 let poco = prompt('Zaglądasz tu ponieważ?',"");
 alert(`${poco}...... dobrze wiedzieć ;p`);
 }
 
+// Alert #4
 function alert4(){
 let isDev= confirm("Jesteś developerem?");
 alert(isDev);
 }
+
+
+/* Kalkulator */
+function Calculator(operator) 
+{
+    var a = document.getElementById('a').value;
+    var b = document.getElementById('b').value;  
+ 
+    switch(operator) 
+    {
+        case '+':
+            var result = parseFloat(a) + parseFloat(b);         
+        break;
+        case '-':
+            var result = parseFloat(a) - parseFloat(b);         
+        break;  
+        case 'x':
+            var result = parseFloat(a) * parseFloat(b);         
+        break;  
+        case '/':
+            var result = parseFloat(a) / parseFloat(b);         
+        break;
+        case '%':
+            var result = parseFloat(a) % parseFloat(b);         
+        break;
+        case '**':
+            var result = parseFloat(a) ** parseFloat(b);         
+        break;    
+    }
+ 
+    document.getElementById('result').value = '= ' + result;
+}
+
+/* Popup for Kalkulator*/
