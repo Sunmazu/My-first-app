e7ab8b4fbabfd960d81faf3f7592e81367f90a28
Sunmazu <sunm4zu@gmail.com>
2021-04-25 21:51:02 +0200

    pierwsza zmiana - nauka JS #1

---
+++ project-1.js
@@ -0,0 +1,9 @@
+// Alert #1 
+alert('Witajcie');
+alert('Oto mój pierwszy projekt');
+
+let zdanie='nazywam się Matthiass i mam '
+let age = 20;
+let zdanie2=' lat, mam nadzieję, że nie załamałeś się wchodząc i widząc tylko alerty'
+alert(zdanie+age+zdanie2);
+
