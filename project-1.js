02eaae3c2d154acac40f2397e540e6632af998bc
Sunmazu <sunm4zu@gmail.com>
2021-04-25 22:52:45 +0200

    JS lesson#2

--- project-1.js
+++ project-1.js
@@ -1,9 +1,23 @@
 // Alert #1 
-alert('Witajcie');
-alert('Oto mój pierwszy projekt');
+function alert1(){
+    alert('Witaj');
+    alert('Oto mój pierwszy projekt');
+}
 
-let zdanie='nazywam się Matthiass i mam '
+
+function alert2(){
+let zdanie='nazywam się Matthiass i mam ';
 let age = 20;
-let zdanie2=' lat, mam nadzieję, że nie załamałeś się wchodząc i widząc tylko alerty'
+let zdanie2=' lat, mam nadzieję, że nie załamałeś się wchodząc i widząc tyle alertów';
 alert(zdanie+age+zdanie2);
+}
 
+function alert3(){
+let poco = prompt('Zaglądasz tu ponieważ?',"");
+alert(`${poco}...... dobrze wiedzieć ;p`);
+}
+
+function alert4(){
+let isDev= confirm("Jesteś developerem?");
+alert(isDev);
+}
